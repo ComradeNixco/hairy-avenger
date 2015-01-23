13c8bfe0b067e36b35a76f2f768cc08d6fdeed5a
Nicolas Lapointe <nixcocpp@gmail.com>
2015-01-23 12:33:09 -0500

    testr

---
+++ ConsoleApplication1/Étudiant.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+	/// <summary>
+	/// Définit un étudiant
+	/// </summary>
+	private class Étudiant
+	{
+		#region Champs
+
+		private string _prénom;
+		private string _nom;
+		private string _DA;
+		private byte _âge;
+		private float _moyenneGénérale;
+		private byte _nbrAbsences;
+		private List<string> listeCours;
+
+		#endregion
+
+		#region Constructeurs/Déstructeurs
+		/// <summary>
+		/// Constructeur par défaut
+		/// </summary>
+		Étudiant()
+		{
+
+		}
+
+		#endregion
+	}
+}
